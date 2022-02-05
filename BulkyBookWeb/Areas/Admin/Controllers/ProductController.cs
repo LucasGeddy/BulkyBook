049916068775b32f00ed79492f476bbdb3634bec
Lucas Sussermann <Sussermann@gmail.com>
2022-02-05 03:39:30 -0300

    Product CRUD finalized

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,5 @@
 ﻿#nullable disable
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using BulkyBook.Data.Repository.Interface;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using BulkyBook.Models.ViewModels;
@@ -21,7 +20,7 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         {
-            return View(_unit.Product.GetAll());
+            return View();
         }
 
         public IActionResult Upsert(int? id)
@@ -53,6 +52,15 @@ namespace BulkyBookWeb.Controllers
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extension = Path.GetExtension(file.FileName);
 
+                    if (productVM.product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStreams = new FileStream(Path.Combine(uploads, fileName+extension), FileMode.Create))
                     {
                         file.CopyTo(fileStreams);
@@ -60,36 +68,60 @@ namespace BulkyBookWeb.Controllers
                     productVM.product.ImageUrl = @"\images\products\" + fileName + extension;
                 }
 
-                _unit.Product.Add(productVM.product);
+                if (productVM.product.Id != 0)
+                {
+                    _unit.Product.Update(productVM.product);
+                    TempData["success"] = "Product updated succesfully!";
+                }
+                else
+                {
+                    _unit.Product.Add(productVM.product);
+                    TempData["success"] = "Product created succesfully!";
+                }
                 await _unit.Save();
-                TempData["success"] = "Product created succesfully!";
+                
                 return RedirectToAction(nameof(Index));
             }
 
             var returnVM = InitViewModel();
             returnVM.product = productVM.product;
+            TempData["error"] = "ERROR: Product was not added/updated!";
             return View(returnVM);
         }
-
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var productList = _unit.Product.GetAll(includeProperties: "Category");
+            return Json(new { data = productList });
+        }
+        [HttpDelete]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
-                return NotFound();
+                return Json(new { success = false, message = "Error deleting product. ID wasn't passed to the API" });
             }
 
             var product = _unit.Product.GetFirstOrDefault(r => r.Id == id);
             if (product == null)
             {
-                return NotFound();
+                return Json(new { success = false, message = "Error deleting product. NotFound" });
+            }
+
+            var imagePath = Path.Combine(_webHost.WebRootPath, product.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
             }
 
             _unit.Product.Remove(product);
             await _unit.Save();
 
-            return RedirectToAction(nameof(Index));
+            return Json(new { success = true, message = "Product deleted successfully!" });
         }
-
+        #endregion
+        #region Helper Functions
         private bool ProductExists(int id)
         {
             return _unit.Product.GetAll().Any(e => e.Id == id);
@@ -112,5 +144,6 @@ namespace BulkyBookWeb.Controllers
                 }),
             };
         }
+        #endregion
     }
 }
