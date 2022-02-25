2983fb930a1784215961bbe5eea0037695ed3881
Lucas Sussermann <sussermann@gmail.com>
2022-02-25 00:38:47 -0300

    Shopping Cart created

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using BulkyBook.Models;
+﻿using BulkyBook.Data.Repository.Interface;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,30 @@ namespace BulkyBookWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unit;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unit)
         {
             _logger = logger;
+            _unit = unit;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unit.Product.GetAll(includeProperties: "Category,CoverType");
+            
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cart = new()
+            {
+                Count = 1,
+                Product = _unit.Product.GetFirstOrDefault(r => r.Id == id, includeProperties: "Category,CoverType")
+            };
+
+            return View(cart);
         }
 
         public IActionResult Privacy()
